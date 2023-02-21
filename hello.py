83e3c28bf83230f8dcf5f8209847d5d2d4d9bc30
SE-KIRIRO <lawrencekiriro@gmail.com>
2023-02-21 08:43:43 +0300

    initial application

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>hello world</h1>"
+
+@app.route("/user<name>")
+def user(name):
+    return f"hello there {name}! "
