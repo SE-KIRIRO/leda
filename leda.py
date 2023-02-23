f1bca7dc2de2973ac288139cc30db4dd4ba284b6
SE-KIRIRO <lawrencekiriro@gmail.com>
2023-02-23 08:04:37 +0300

    gitignore

--- leda.py
+++ leda.py
@@ -1,7 +1,10 @@
 import os
+import click
 from app import create_app, db
 from app.models import User, Role
 from flask_migrate import Migrate
+
+
 
 app = create_app(os.getenv("FLASK_CONFIG", "default"))
 migrate = Migrate(app, db)
@@ -10,3 +13,13 @@ migrate = Migrate(app, db)
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
 
+@app.cli.command()
+@click.argument("test_names", nargs=-1)
+def test(test_names):
+    """run the unittests"""
+    import unittest
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
